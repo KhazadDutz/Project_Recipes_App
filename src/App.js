fdfd5fc15f0af9ff2034c3b95698de14592a9e3a
Mauricio <mauricio.s.ieiri@gmail.com>
2023-01-09 18:30:27 -0300

    chore: adiciona projeto completo em um novo repositorio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Routes from './routes/Routes';
+
+function App() {
+  return (
+    <div className="app">
+      <Routes />
+    </div>
+  );
+}
+export default App;
